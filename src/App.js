0eda568d526b522eb66b944ec02ef6cd6c0f725c
unknown <alseanupama0@gmail.com>
2023-04-25 11:22:22 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+     <div className="wrap">
+      <div className="center">
+        <div className="candle">
+        <div className="cflame">  &nbsp; </div>
+          <div className="cwick">   &nbsp;   </div>
+            <div className="cwax">   &nbsp;   </div>
+            <div className="cstand">   &nbsp;    </div>
+            </div>
+            </div>
+            </div>
+   
   );
 }
 
